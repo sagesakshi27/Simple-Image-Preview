90e0d964c5acc9e5c7e8481ffd3a19c6741e6cdb
Sakshi Chavan <sakshichavan4151@gmail.com>
2026-05-06 13:02:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+const imageInput = document.getElementById('ImageInput');
+const imagePreview = document.getElementById('ImagePreview');
+imageInput.addEventListener('change', () => {
+  const file = imageInput.files[0];
+  if (file) {
+    imagePreview.src = URL.createObjectURL(file);
+    imagePreview.style.display = 'block';
+  }
+});
